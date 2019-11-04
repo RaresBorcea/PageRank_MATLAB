2dfa5bede4d6c5a55894ee8f493a3502a9b84121
Rares Borcea <raresborcea@gmail.com>
2019-11-04 21:38:36 +0200

    Update Apartenenta.m

--- Apartenenta.m
+++ Apartenenta.m
@@ -1,5 +1,3 @@
-% Borcea Rares Ioan, 314CC
-
 function y = Apartenenta(x, val1, val2)
 	% Functia care primeste ca parametrii x, val1, val2 si care calculeaza valoarea functiei membru in punctul x.
 	% Stim ca 0 <= x <= 1
